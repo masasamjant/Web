f7eff3906b0f29bd97f3407be8ebbe376f3f13a6
Matti Jantti <m_jantti@hotmail.com>
2025-10-30 23:04:58 +0200

    Create route provider interface. Add license and readme files.

---
+++ Masasamjant.Web/Routing/IRouteProvider.cs
@@ -0,0 +1,16 @@
+﻿namespace Masasamjant.Web.Routing
+{
+    /// <summary>
+    /// Represents provider of routes.
+    /// </summary>
+    public interface IRouteProvider
+    {
+        /// <summary>
+        /// Gets the route specified by name.
+        /// </summary>
+        /// <param name="name">The route name.</param>
+        /// <returns>A route specified by name or empty string.</returns>
+        /// <exception cref="ArgumentNullException">If value of <paramref name="name"/> is empty or only whitespace.</exception>
+        string GetRoute(string name);
+    }
+}
